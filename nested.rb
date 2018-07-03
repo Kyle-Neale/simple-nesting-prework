aecb375699cfe72c45dfb2157d914bca0ea7a5dc
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-03 15:11:51 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -84,6 +84,7 @@ def adding_matz
   programmer_hash[:yukihiro_matsumoto] = {}
   programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
   programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP, C"]
+  programmer_hash
     
 end
 
@@ -105,8 +106,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
-     
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +127,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 
 end
